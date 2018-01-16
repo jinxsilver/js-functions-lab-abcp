bfc87e9f9e79d7e7a8572b7a419cce3b8216492c
rey soto <jinxsilver@gmail.com>
2018-01-16 03:37:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidays() {
 happyHolidays()
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+  console.log('Happy holidays, ${name}!')
 };
 
 happyHolidaysTo("you")
